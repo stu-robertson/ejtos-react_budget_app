6bbb956bf32010f457fefe7a646a446c2526d1a9
stu-robertson <stu.robertson.development@gmail.com>
2023-10-12 11:26:30 -0400

    styled currency drop down

--- src/components/CurrencySelector.js
+++ src/components/CurrencySelector.js
@@ -1,30 +1,69 @@
 import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 const Currency = () => {
-    const { currency } = useContext(AppContext);
-    const { dispatch } = useContext(AppContext);
-    const [newCurrency, setNewCurrency] = useState(currency);
-    const handleCurrencyChange = (event) => {      
-        const newCurrency = event.target.value;
-        setNewCurrency(newCurrency);
-        dispatch({ 
-            type:'CHG_CURRENCY', 
-            payload: newCurrency,
-       });
+    const [isActive, setIsActive] = useState(false);
+    const [selected, setIsSelected] = useState("Choose one");
+    function handleBlur(e) {
+      console.log(e);
     }
-
-
     return (
-<div className='custom-dropdown'>
-<span>Currency: {currency}</span>
-<select id="currency" value={currency} onChange={handleCurrencyChange}>
-    <option value="$">$ Dollar</option>
-    <option value="£">£ Pound</option>
-    <option value="€">€ Euro</option>
-    <option value="₹">₹ Ruppee</option>
-</select>
-
-</div>
+      <div className="App">
+        <div className="dropdown">
+          <div
+            onClick={(e) => {
+              setIsActive(!isActive);
+            }}
+            className="dropdown-btn"
+          >
+            Currency ({selected}) ▼
+            <span
+              className={isActive ? "fas fa-caret-up" : "fas fa-caret-down"}
+            />
+          </div>
+          <div
+            className="dropdown-content"
+            style={{ display: isActive ? "block" : "none" }}
+          >
+            <div
+              onClick={(e) => {
+                setIsSelected(e.target.textContent);
+                setIsActive(!isActive);
+              }}
+              className="item"
+            >
+              $ Dollar
+            </div>
+            <div
+              className="item"
+              onClick={(e) => {
+                setIsSelected(e.target.textContent);
+                setIsActive(!isActive);
+              }}
+            >
+              £ Pound
+            </div>
+            <div
+              className="item"
+              onClick={(e) => {
+                setIsSelected(e.target.textContent);
+                setIsActive(!isActive);
+              }}
+            >
+              € Euro
+            </div>
+            <div
+              className="item"
+              onClick={(e) => {
+                setIsSelected(e.target.textContent);
+                setIsActive(!isActive);
+              }}
+            >
+              ₹ Ruppee
+            </div>
+          </div>
+        </div>
+      </div>
     );
+   
 };
 export default Currency;
