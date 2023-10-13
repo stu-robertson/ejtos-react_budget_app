d521c6f548b3c09aa42c378f7cc3880faa175566
stu-robertson <stu.robertson.development@gmail.com>
2023-10-13 16:06:17 -0400

    icons styled

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,10 @@
 import React, { useContext } from 'react';
-import { TiDelete, TiPlusOutline } from 'react-icons/ti';
+import { IconContext } from 'react-icons';
+import { TiDelete, TiPlus, TiMinus } from 'react-icons/ti';
 import { AppContext } from '../context/AppContext';
+
 const ExpenseItem = (props) => {
+    let circleClasses = "inline-block p-7 rounded-full w-20 mx-auto";
     const { currency } = useContext(AppContext);
     const { dispatch } = useContext(AppContext);
     const handleDeleteExpense = () => {
@@ -34,9 +37,15 @@ const ExpenseItem = (props) => {
                 <tr>
                 <td>{props.name}</td>
                 <td>{currency}{props.cost}</td>
-                <td><button className="increase-button"onClick={event=> increaseAllocation(props.name)}>+</button></td>
-                <td><button className="decrease-button"onClick={event=> decreaseAllocation(props.name)}>-</button></td>
+                <IconContext.Provider value={{ color: "white", className: "plus-icon" }}>
+                <td><TiPlus size='1.5em'onClick={event=> increaseAllocation(props.name)}></TiPlus></td>
+                </IconContext.Provider>
+                <IconContext.Provider value={{ color: "white", className: "minus-icon" }}>
+                <td><TiMinus size='1.5em' onClick={event=> decreaseAllocation(props.name)}>-</TiMinus></td>
+                </IconContext.Provider>
+                <IconContext.Provider value={{ color: "red", className: "global-class-name" }}>
                 <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
+                </IconContext.Provider>
                 </tr>
     );
 };
