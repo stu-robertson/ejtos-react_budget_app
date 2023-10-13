d62cd8b5bc2813183acc44289bfe7d5a2f895ef6
stu-robertson <stu.robertson.development@gmail.com>
2023-10-13 11:59:56 -0400

    functional but not formatted inc and dec buttons

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { TiDelete } from 'react-icons/ti';
+import { TiDelete, TiPlusOutline } from 'react-icons/ti';
 import { AppContext } from '../context/AppContext';
 const ExpenseItem = (props) => {
     const { currency } = useContext(AppContext);
@@ -20,11 +20,22 @@ const ExpenseItem = (props) => {
             payload: expense
         });
     }
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: 10,
+        };
+        dispatch({
+            type: 'RED_EXPENSE',
+            payload: expense
+        });
+    }
     return (
                 <tr>
                 <td>{props.name}</td>
                 <td>{currency}{props.cost}</td>
-                <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
+                <td><button className="increase-button"onClick={event=> increaseAllocation(props.name)}>+</button></td>
+                <td><button className="decrease-button"onClick={event=> decreaseAllocation(props.name)}>-</button></td>
                 <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
                 </tr>
     );
